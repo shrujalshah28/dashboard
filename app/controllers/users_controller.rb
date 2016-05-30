8fb0989aca64e23103b5f660b023a85d215a9412
Shrujal Shah <shrujalshah@hotmail.com>
2016-05-30 21:42:56 +0000

    edit update funtionality and partial

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,6 +15,22 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+      
+    if @user.update(user_params)
+      flash[:success] = "Your account was updated successfullly"
+      #redrict_to users_path
+      render 'new'
+    else
+      render 'edit'  
+    end
+  end
+
   private
   def user_params
     params.require(:user).permit(:username, :email, :password)
