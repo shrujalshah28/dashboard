075914e88664e76cb1a66bc9e9ebcb10118e5780
Shrujal Shah <shrujalshah@hotmail.com>
2016-05-31 22:03:53 +0000

    Add admin user funtionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -42,8 +42,10 @@ class UsersController < ApplicationController
   end
 
   def destroy
+    if(@user.id == current_user.id)
+      session[:user_id] = nil
+    end
     @user.destroy
-    session[:user_id] = nil
     flash[:danger] = "User was successfully delected"
     redirect_to users_path
   end
@@ -58,7 +60,7 @@ class UsersController < ApplicationController
   end
 
   def require_same_user
-    if current_user != @user
+    if current_user != @user and !current_user.admin?
       flash[:danger] = "You can only edit or delete your own profile"
       redirect_to root_path
     end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160530180840) do
+ActiveRecord::Schema.define(version: 20160531210616) do
 
   create_table "users", force: :cascade do |t|
     t.string   "username"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20160530180840) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "password_digest"
+    t.boolean  "admin",           default: false
   end
 
 end
