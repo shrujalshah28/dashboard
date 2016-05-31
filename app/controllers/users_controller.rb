bf1656d217a3d035532c03baf5259064ce9bef20
Shrujal Shah <shrujalshah@hotmail.com>
2016-05-31 21:03:24 +0000

    Add sing in upon sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,8 +16,9 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
+      session[:user_id] = @user.id
       flash[:success] = "Welcome to Dashboard #{@user.username}"
-      redirect_to user_path(user)
+      redirect_to user_path(@user)
       #render 'new'
     else
       render 'new'
